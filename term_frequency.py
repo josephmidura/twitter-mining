0d8d64e8d8921f66191449f994b3b4faddd79738
Joe Midura <josephmidura@utexas.edu>
2018-02-21 10:56:10 -0600

    initial commit

---
+++ term_frequency.py
@@ -0,0 +1,36 @@
+import operator 
+import json
+from collections import Counter
+ 
+fname = 'mytweets.json'
+with open(fname, 'r') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # Create a list with all the terms
+        terms_all = [term for term in preprocess(tweet['text'])]
+        # Update the counter
+        count_all.update(terms_all)
+    # Print the first 5 most frequent words
+    print(count_all.most_common(5))
+
+# Remove stop words
+from nltk.corpus import stopwords
+import string
+ 
+punctuation = list(string.punctuation)
+stop = stopwords.words('english') + punctuation + ['rt', 'via']
+
+# Count terms only once, equivalent to Document Frequency
+terms_single = set(terms_all)
+# Count hashtags only
+terms_hash = [term for term in preprocess(tweet['text']) 
+              if term.startswith('#')]
+# Count terms only (no hashtags, no mentions)
+terms_only = [term for term in preprocess(tweet['text']) 
+              if term not in stop and
+              not term.startswith(('#', '@'))] 
+              # mind the ((double brackets))
+              # startswith() takes a tuple (not a list) if 
+              # we pass a list of inputs
+
